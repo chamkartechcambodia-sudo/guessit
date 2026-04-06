3a68bf45c20e96654ec38c0043f3dc78e45c4c12
chamkartechcambodia-sudo <chamkartechcambodia@gmail.com>
2026-04-06 21:55:16 +0700

    chore: Step.05-Exercise-Add-End-Game-Event

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,58 +1,14 @@
 package com.example.android.guesstheword.screens.game
 
-import android.os.CountDownTimer
-import android.text.format.DateUtils
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
-private val CORRECT_BUZZ_PATTERN = longArrayOf(100, 100, 100, 100, 100, 100)
-private val PANIC_BUZZ_PATTERN = longArrayOf(0, 200)
-private val GAME_OVER_BUZZ_PATTERN = longArrayOf(0, 2000)
-private val NO_BUZZ_PATTERN = longArrayOf(0)
 
 /**
  * ViewModel containing all the logic needed to run the game
  */
 class GameViewModel : ViewModel() {
-
-    // These are the three different types of buzzing in the game. Buzz pattern is the number of
-    // milliseconds each interval of buzzing and non-buzzing takes.
-    enum class BuzzType(val pattern: LongArray) {
-        CORRECT(CORRECT_BUZZ_PATTERN),
-        GAME_OVER(GAME_OVER_BUZZ_PATTERN),
-        COUNTDOWN_PANIC(PANIC_BUZZ_PATTERN),
-        NO_BUZZ(NO_BUZZ_PATTERN)
-    }
-
-    companion object {
-        // These represent different important times in the game, such as game length.
-
-        // This is when the game is over
-        private const val DONE = 0L
-
-        // This is the time when the phone will start buzzing each second
-        private const val COUNTDOWN_PANIC_SECONDS = 10L
-
-        // This is the number of milliseconds in a second
-        private const val ONE_SECOND = 1000L
-
-        // This is the total time of the game
-        private const val COUNTDOWN_TIME = 60000L
-
-    }
-
-    private val timer: CountDownTimer
-
-    private val _currentTime = MutableLiveData<Long>()
-    val currentTime: LiveData<Long>
-        get() = _currentTime
-
-    // The String version of the current time
-    val currentTimeString = Transformations.map(currentTime) { time ->
-        DateUtils.formatElapsedTime(time)
-    }
 
     // The current word
     private val _word = MutableLiveData<String>()
@@ -69,39 +25,13 @@ class GameViewModel : ViewModel() {
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
 
-    // Event which triggers the end of the game
-    private val _eventGameFinish = MutableLiveData<Boolean>()
-    val eventGameFinish: LiveData<Boolean>
-        get() = _eventGameFinish
-
-    // Event that triggers the phone to buzz using different patterns, determined by BuzzType
-    private val _eventBuzz = MutableLiveData<BuzzType>()
-    val eventBuzz: LiveData<BuzzType>
-        get() = _eventBuzz
+    // TODO (01) Make a properly encapsulated LiveData called eventGameFinish that holds a
+    // boolean
 
     init {
         resetList()
         nextWord()
         _score.value = 0
-
-        // Creates a timer which triggers the end of the game when it finishes
-        timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND) {
-
-            override fun onTick(millisUntilFinished: Long) {
-                _currentTime.value = (millisUntilFinished / ONE_SECOND)
-                if (millisUntilFinished / ONE_SECOND <= COUNTDOWN_PANIC_SECONDS) {
-                    _eventBuzz.value = BuzzType.COUNTDOWN_PANIC
-                }
-            }
-
-            override fun onFinish() {
-                _currentTime.value = DONE
-                _eventBuzz.value = BuzzType.GAME_OVER
-                _eventGameFinish.value = true
-            }
-        }
-
-        timer.start()
     }
 
     /**
@@ -140,9 +70,11 @@ class GameViewModel : ViewModel() {
     private fun nextWord() {
         //Select and remove a word from the list
         if (wordList.isEmpty()) {
-            resetList()
+            // gameFinished() should happen here
+            // TODO (03) Set eventGameFinish to true, to signify that the game is over
+        } else {
+            _word.value = wordList.removeAt(0)
         }
-        _word.value = wordList.removeAt(0)
     }
 
     /** Methods for buttons presses **/
@@ -154,22 +86,9 @@ class GameViewModel : ViewModel() {
 
     fun onCorrect() {
         _score.value = (_score.value)?.plus(1)
-        _eventBuzz.value = BuzzType.CORRECT
         nextWord()
     }
 
-    /** Methods for completed events **/
-
-    fun onGameFinishComplete() {
-        _eventGameFinish.value = false
-    }
-
-    fun onBuzzComplete() {
-        _eventBuzz.value = BuzzType.NO_BUZZ
-    }
-
-    override fun onCleared() {
-        super.onCleared()
-        timer.cancel()
-    }
+    // TODO (02) Make the function onGameFinishComplete which makes the value of eventGameFinish
+    // false
 }
