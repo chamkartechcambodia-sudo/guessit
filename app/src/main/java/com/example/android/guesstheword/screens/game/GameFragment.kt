aa67aa2debc689a18b3e20c2f5f395a627ef3d74
chamkartechcambodia-sudo <chamkartechcambodia@gmail.com>
2026-04-06 21:55:46 +0700

    chore: Step.11-Exercise-Optional-Add-Buzzing

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -1,13 +1,9 @@
 package com.example.android.guesstheword.screens.game
 
-import android.os.Build
 import android.os.Bundle
-import android.os.VibrationEffect
-import android.os.Vibrator
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.getSystemService
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -57,31 +53,13 @@ class GameFragment : Fragment() {
             }
         })
 
-        // Buzzes when triggered with different buzz events
-        viewModel.eventBuzz.observe(viewLifecycleOwner, Observer { buzzType ->
-            if (buzzType != GameViewModel.BuzzType.NO_BUZZ) {
-                buzz(buzzType.pattern)
-                viewModel.onBuzzComplete()
-            }
-        })
+        // TODO (09) Created an observer for the buzz event which calls the buzz method with the
+        // correct pattern. Remember to call onBuzzComplete!
 
         return binding.root
 
     }
 
-    /**
-     * Given a pattern, this method makes sure the device buzzes
-     */
-    private fun buzz(pattern: LongArray) {
-        val buzzer = activity?.getSystemService<Vibrator>()
-        buzzer?.let {
-            // Vibrate for 500 milliseconds
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                buzzer.vibrate(VibrationEffect.createWaveform(pattern, -1))
-            } else {
-                //deprecated in API 26
-                buzzer.vibrate(pattern, -1)
-            }
-        }
-    }
+    // TODO (08) Copy over the buzz method here
+
 }
